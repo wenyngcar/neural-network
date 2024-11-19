2912cd43d1cdfa9877c8c5ac50dd5fc4062d72d3
wenyngcar <coconut_rain@users.noreply.github.com>
2024-11-19 21:41:49 +0800

    feat: print feedforward for neural network

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from neuron import Neuron
+from neural_network import NeuralNetwork
 import numpy as np
 
-weights = np.array([0, 1]) 
+weights = np.array([0, 1])
 bias = 4
 n = Neuron(weights, bias)
 
 x = np.array([2, 3])
-print(n.feed_forward(x))
+network = NeuralNetwork()
+
+print(n.feedforward(x))
+print(network.feedforward(x))
