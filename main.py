176f5a29ea08c0831095d1e621a2fa665d5cfba4
wenyngcar <coconut_rain@users.noreply.github.com>
2024-11-19 22:21:52 +0800

    feat: print Mean Sqaured Error

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from neuron import Neuron
 from neural_network import NeuralNetwork
+from mse_loss import mse_loss
 import numpy as np
 
 weights = np.array([0, 1])
@@ -8,6 +9,9 @@ n = Neuron(weights, bias)
 
 x = np.array([2, 3])
 network = NeuralNetwork()
+y_true = np.array([1, 0, 0, 1])
+y_pred = np.array([0, 0, 0, 0])
 
-print(n.feedforward(x))
-print(network.feedforward(x))
+print(f"Neuron feedforward: {n.feedforward(x)}")
+print(f"Neural Network: {network.feedforward(x)}")
+print(f"Mean Squared Error: {mse_loss(y_true, y_pred)}")
