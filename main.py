e826ff099843324d4eac76aee39104cc18618460
wenyngcar <coconut_rain@users.noreply.github.com>
2024-11-26 16:59:48 +0800

    refactor: print epoch neural train function

--- main.py
+++ main.py
@@ -12,6 +12,18 @@ network = NeuralNetwork()
 y_true = np.array([1, 0, 0, 1])
 y_pred = np.array([0, 0, 0, 0])
 
+# Define dataset
+data = np.array([
+    [-2, -1],
+    [25, 6],
+    [17, 4],
+    [-15, -6]
+])
+
+all_y_trues = np.array([1, 0, 0, 1])
+
+network.train(data, all_y_trues)
+
 print(f"Neuron feedforward: {n.feedforward(x)}")
 print(f"Neural Network: {network.feedforward(x)}")
 print(f"Mean Squared Error: {mse_loss(y_true, y_pred)}")
