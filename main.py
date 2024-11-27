4c16ef74400315d7910048137afc9aed0db7943d
wenyngcar <coconut_rain@users.noreply.github.com>
2024-11-27 14:52:19 +0800

    refactor: make predictions with 2 variable male and female

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ weights = np.array([0, 1])
 bias = 4
 n = Neuron(weights, bias)
 
-x = np.array([2, 3])
 network = NeuralNetwork()
 y_true = np.array([1, 0, 0, 1])
 y_pred = np.array([0, 0, 0, 0])
@@ -19,11 +18,19 @@ data = np.array([
     [17, 4],
     [-15, -6]
 ])
-
 all_y_trues = np.array([1, 0, 0, 1])
 
+# Train neural network
 network.train(data, all_y_trues)
+
+x = np.array([2, 3])
+emily = np.array([-7, -3]) # 128 pounds, 63 inches
+frank = np.array([20, 2]) # 155 pounds, 68 inches
 
 print(f"Neuron feedforward: {n.feedforward(x)}")
 print(f"Neural Network: {network.feedforward(x)}")
 print(f"Mean Squared Error: {mse_loss(y_true, y_pred)}")
+
+# If closer to one, then is a female.
+print("Emily: %.3f" % network.feedforward(emily))
+print("Frank: %.3f" % network.feedforward(frank))
